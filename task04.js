383196236f71e5210390340939d4f180ac420aed
nuttapatgd <nuttapat.gd@gmail.com>
2025-07-15 21:44:18 +0700

    task04 solution

--- task04.js
+++ task04.js
@@ -17,7 +17,21 @@ Task 04: Traffic Light Action
 */
 
 // 👨‍💻 TODO-01: Write your code below to complete the task as described above.
-
+function trafficLightAction(color) {
+    switch (color) {
+        case "green":
+            return "Go";
+            break;
+        case "yellow":
+            return "Slow down";
+            break;
+        case "red":
+            return "Stop";
+            break;
+        default:
+            return "Invalid light color";
+    }
+}
 // Example Test Cases
 console.log(trafficLightAction("green")); // "Go"
 console.log(trafficLightAction("yellow")); // "Slow down"
